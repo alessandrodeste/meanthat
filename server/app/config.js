3cf7bf9533b5e6254906e69ae92011bf988d2cf9
Alessandro d'Este <alessandrodeste@gmail.com>
2014-09-01 10:28:01 +0000

    server configuration changes

--- server/app/config.js
+++ server/app/config.js
@@ -3,20 +3,19 @@ var path = require('path');
 module.exports = {
   mongo: {
     dbUrl: 'https://api.mongolab.com/api/1',            // The base url of the MongoLab DB server
-    apiKey: '16NtN8-wSxdVnzJspLvwzQU2iE9HgAHW'          // Our MongoLab API key
+    apiKey: '167898-9843fsdfds6f54dsf689HgAHW'          // Our MongoLab API key
   },
   security: {
-    dbName: 'nodethatdb',                               // The name of database that contains the security information
+    dbName: 'meanthatdb',                               // The name of database that contains the security information
     usersCollection: 'users'                            // The name of the collection contains user information
   },
   server: {
     listenPort: process.env.PORT || 3000,               // The port on which the server is to listen (means that the app is at http://localhost:3000 for instance)
     securePort: 8433,                                   // The HTTPS port on which the server is to listen (means that the app is at https://localhost:8433 for instance)
     ip: process.env.IP || "0.0.0.0",
-    distFolder: path.resolve(__dirname, '../public/'),  // The folder that contains the application files (note that the files are in a different repository) - relative to this file
+    distFolder: path.resolve(__dirname, '../../client/dist/'),  // The folder that contains the application files (note that the files are in a different repository) - relative to this file
     staticUrl: '/',                               // The base url from which we serve static files (such as js, css and images)
     cookieSecret: 'meanthat'                         // The secret for encrypting the cookie
-    
   },
   
   external_api: {
--- server/server.js
+++ server/server.js
@@ -7,16 +7,16 @@ var express      = require('express');
 var app          = express(); 				
 var bodyParser   = require('body-parser');
 var cookieParser = require('cookie-parser');
+var session      = require('express-session');
 var mongoose     = require('mongoose');
-var path         = require('path'); 
 var config 			 = require('./app/config.js');
-var security     = require('./app/security/security');
-var protectJSON  = require('./app/security/protectJSON');
 var passport     = require('passport');
 var morgan       = require('morgan');
 var flash        = require('connect-flash');
-//var xsrf         = require('./app/security/xsrf');
-require('express-namespace');
+//var protectJSON  = require('./app/security/protectJSON');
+//var path         = require('path'); 
+//var xsrf       = require('./app/security/xsrf');
+//var namespace    = require('express-namespace');
 
 app.use(morgan('dev'));                                     // log every request to the console
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,18 +24,25 @@ app.use(bodyParser.json());
 // app.use(protectJSON); // is needed?!
 
 app.use(flash());
-app.use(express.cookieParser(config.server.cookieSecret));  // Hash cookies with this secret
-app.use(express.cookieSession());                           // Store the session in the (secret) cookie
+app.use(cookieParser(config.server.cookieSecret));                                    // read cookies (needed for auth)
+app.use(session({ secret: config.server.cookieSecret }));   // session secret
 app.use(passport.initialize());                             // Initialize PassportJS
 app.use(passport.session());                                // Use Passport's session authentication strategy - this stores the logged in user in the session and will now run on any request
 //app.use(xsrf); // is needed??!                            // Add XSRF checks to the request
-security.initialize(config.mongo.dbUrl, config.mongo.apiKey, config.security.dbName, config.security.usersCollection); // Add a Mongo strategy for handling the authentication
+
+// init passport strategies
+require('./app/security/strategies')(passport);
 
 // connect mongodb
-mongoose.connect('mongodb://localhost/nodethatdb'); // connect to our database
+// TODO: move to mongolab 
+// mongoose.connect('mongodb://localhost/nodethatdb'); // connect to our database
 
 // Routing
 // =============================================================================
+app.get('/ping', function(req, res) {
+  console.log("ping");
+  res.json({message: "pong"});
+});
 // Security checks
 app.all('/api/security', function(req, res, next) {
   console.log('check /api/security');
@@ -51,13 +58,13 @@ app.all('/*', function(req, res, next) {
   console.log('check /*');
   // We require the user is authenticated to execute any action except login
   //security.authenticationRequired(req, res, next);
+  next();
 });
 
 // Routing around
 require('./app/routes/appFile')(app, config);
 require('./app/routes/static')(app, config);
-// app.* style
-app.namespace('/api/security', function () { require('./app/routes/security')(app, config); });
+require('./app/routes/security')(app, config);
 // route style
 app.use('/api/tasks',            require('./app/routes/tasks'));
 app.use('/api/tasks',            require('./app/routes/task'));
@@ -69,7 +76,6 @@ app.all('/*', function(req, res) {
   // Just send the index.html for other files to support HTML5Mode
   res.sendfile('index.html', { root: config.server.distFolder });
 });
-
 
 // START THE SERVER
 // =============================================================================
@@ -83,15 +89,12 @@ app.listen(config.server.listenPort, config.server.ip, function(){
 });
 
 
-
-
-
-// TODO: https://blog.liftsecurity.io/tag/headers
-
 /*
 
 per la sicurezza: 
 
+https://blog.liftsecurity.io/tag/headers
+ 
 https://github.com/strongloop/express/blob/master/examples/route-middleware/index.js
 
 https://github.com/strongloop/express/blob/master/examples/auth/app.js
